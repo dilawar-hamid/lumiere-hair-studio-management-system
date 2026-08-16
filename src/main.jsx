46d20d34a9f4723eb76e67d2b1b1f04c1f5cca9e
Dilawar <dilawarhamid446@gmail.com>
2026-08-16 14:20:59 -0700

    Fix responsive layout and horizontal scroll

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App.jsx'
+import './index.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter } from 'react-router-dom'
 
